2a8a26d58f1d5d244815d066ce0ecf005d0b3137
Chris Zuber <shgysk8zer0@gmail.com>
2021-03-22 14:18:51 -0700

    Collect names and ages for derby registration

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,80 @@ import { uuidv6 } from 'https://cdn.kernvalley.us/js/std-js/uuid.js';
 import { supportedInstruments, registrationCost } from './consts.js';
 import { getCustomElement } from 'https://cdn.kernvalley.us/js/std-js/custom-elements.js';
 import { preload, loadScript } from 'https://cdn.kernvalley.us/js/std-js/loader.js';
-import { create } from 'https://cdn.kernvalley.us/js/std-js/dom.js';
+import { create, on, off, attr } from 'https://cdn.kernvalley.us/js/std-js/dom.js';
+
+async function getRegistered({ adults, children }) {
+	if (! (Number.isInteger(adults) && Number.isInteger(children) && adults > 0 && children >= 0)) {
+		throw new TypeError('Number of adults and children for registration must be an integer');
+	} else {
+		return await new Promise((resolve, reject) => {
+			const regDialog = document.getElementById('registration-details-dialog');
+			const regForm = document.getElementById('registration-details');
+			const adultTmp = document.getElementById('register-adult-template').content;
+			const childTmp = document.getElementById('register-child-template').content;
+			const adultSection = document.getElementById('register-adult-section');
+			const childSection = document.getElementById('register-child-section');
+
+			const handlers = {
+				submit:  event => {
+					event.preventDefault();
+					const form = event.target;
+					const data = new FormData(form);
+					const names = data.getAll('reg[][name]');
+					const ages = data.getAll('reg[][age]');
+
+					if (names.length !== ages.length) {
+						throw new TypeError('Name/age mismatch');
+					} else {
+						const entries = names.map((name, i) => {
+							return { name, age: parseInt(ages[i]) };
+						});
+
+						resolve(entries);
+
+						form.closest('dialog').close();
+					}
+				},
+				reset: event => {
+					event.target.closest('dialog').close();
+					Array.from(event.target.querySelectorAll('.derby-reg-details')).forEach(el => el.remove());
+					reject(new DOMException('User cancelled registration'));
+				},
+			};
+
+			on(regDialog, {
+				close: ({ target }) => {
+					Array.from(target.querySelectorAll('.derby-reg-details')).forEach(el => el.remove());
+					off(target.querySelectorAll('form'), handlers);
+				}
+			});
+
+			for (let n = 0; n < adults; n++) {
+				const base = adultTmp.cloneNode(true);
+				Promise.allSettled([
+					attr('.reg-name-label', { for: `reg-name-child-${n}`}, { base }),
+					attr('.reg-age-label', { for: `reg-age-child-${n}`}, { base }),
+					attr('.reg-name-input', { id: `reg-name-child-${n}`}, { base }),
+					attr('.reg-age-label', { id: `reg-age-child-${n}`}, { base }),
+				]).then(() => adultSection.append(base));
+			}
+
+			for (let n = 0; n < children; n++) {
+				const base = childTmp.cloneNode(true);
+				Promise.allSettled([
+					attr('.reg-name-label', { for: `reg-name-child-${n}`}, { base }),
+					attr('.reg-age-label', { for: `reg-age-child-${n}`}, { base }),
+					attr('.reg-name-input', { id: `reg-name-child-${n}`}, { base }),
+					attr('.reg-age-label', { id: `reg-age-child-${n}`}, { base }),
+				]).then(() => childSection.append(base));
+			}
+
+			on(regForm, handlers);
+			regDialog.showModal();
+		});
+
+	}
+}
 
 export async function pay(details, opts = {}, fallback = false) {
 	if ('PaymentRequest' in window && fallback === false) {
@@ -72,6 +145,7 @@ export async function derbyRegister({ adults = 1, children = 0 } = {}) {
 	if (! (Number.isInteger(adults) && Number.isInteger(children))) {
 		throw new TypeError('Adults and children must be integers');
 	} else {
+		const reg = await getRegistered({ adults, children });
 		const displayItems = [{
 			label: `Adults (16+): ${adults}`,
 			amount: {
@@ -106,7 +180,7 @@ export async function derbyRegister({ adults = 1, children = 0 } = {}) {
 			children: [
 				create('pre', {
 					children: [
-						create('code', { text: JSON.stringify(resp, null, 4 )}),
+						create('code', { text: JSON.stringify({ reg, resp }, null, 4 )}),
 					]
 				})
 			],
